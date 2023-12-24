b10cb7e077b35669af017a2c4e58c35587ffe05e
olehandro <odpash.itmo@gmail.com>
2023-12-24 16:32:09 +0300

    e

--- scraper.py
+++ scraper.py
@@ -23,6 +23,7 @@ def main():
         accounts = database.get_all_accounts()
         for account in accounts:
             print(account.session_name + '.session')
+            print(account.search_words)
             if not os.path.exists(account.session_name + '.session'):
                 continue
             telegram_client = create_telegram_client(account.session_name, account.api_id, account.api_hash)
